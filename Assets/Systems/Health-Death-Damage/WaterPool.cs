e75fb854f834a22b1296ba1c7e58619ef857e874
Storm Hughes <31044937+BoxheadPixelart@users.noreply.github.com>
2021-07-20 20:53:07 -0500

    water changes

---
+++ Assets/Systems/Health-Death-Damage/WaterPool.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPool : MonoBehaviour
+{
+    public Health hp;
+    public bool isWet;
+    float timer; 
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (isWet)
+        {
+            timer += Time.deltaTime; 
+            if (timer >= .05f)
+            {
+                timer = 0;
+                hp.AddHealth(10); 
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isWet = true;
+        }
+   
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isWet = false;
+        }
+    }
+}
